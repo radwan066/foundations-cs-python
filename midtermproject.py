651edf2bad484af3f8b5a1c1140c74dbe82fa56d
radwan066 <radwansaad066@gmail.com>
2023-11-25 12:11:20 +0200

    adding func Open_Nested_Tabs

--- midtermproject.py
+++ midtermproject.py
@@ -13,7 +13,6 @@ def Open_Tab():   #asking the user for the title and the url of the  website
          openedtabs.append(tabs)
        else :
           url=input("please enter the URL again:")
-          Open_Tab()
     else :      
       print("please input a valid Title!")
       Open_Tab()     
@@ -44,7 +43,19 @@ def Display_All_Tabs(openedtabs):
     for i in openedtabs:
         print(i.get('title'))
         
-
+def Open_Nested_Tabs(openedtabs):
+    index=int(input("please enter the index of the parent tab:"))
+    for index in openedtabs:
+        title=input("give us the title of the tab:")
+        if title and title.isalnum():
+            url=input("please enter the URL:")
+            if url and url.startswith(("www.","http:","https:")):
+                nestedtabs={"title":title,"url":url}
+            else :
+                url=input("please enter the url again:") 
+        else :
+            print("please input a valid Title!")
+            Open_Nested_Tabs(openedtabs)           
 
 def mainmenu():
     choice=0
@@ -68,7 +79,7 @@ def mainmenu():
         elif (choice==4):
             Display_All_Tabs(openedtabs)
         elif(choice==5):
-            Open_Nested_Tabs()
+            Open_Nested_Tabs(openedtabs)
         elif(choice==6):
             Sort_All_Tabs()
         elif(choice==7):
