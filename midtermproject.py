6a285a96b42d2a569017c6c6e5af911520a39cf4
radwan066 <radwansaad066@gmail.com>
2023-12-06 12:19:56 +0200

    adding ass6

--- midtermproject.py
+++ midtermproject.py
@@ -22,7 +22,7 @@ def Open_Tab():   #asking the user for the title and the url of the  website O(n
      
 def Close_Tab():  #first printing the list of the opened tabs than asking user for the index of the tab he wishes to close it #O(n) n:wrong index from the user
     y=(input("please,enter the index of the tabs you want to close:"))  
-    if y.isdigit():
+    if y.isdigit():  
       index=int(y)
       if openedtabs[index] in openedtabs:   #searching for the index given by the user
         openedtabs.remove(openedtabs[index])
@@ -33,12 +33,12 @@ def Close_Tab():  #first printing the list of the opened tabs than asking user f
         Close_Tab()    
     print(openedtabs)
 
-def Switch_Tabs(): #first printing the list of the opened tabs than asking the user for the index of the tab in which he would see its content
+def Switch_Tabs(): #first printing the list of the opened tabs than asking the user for the index of the tab in which he would see its content O(n^2)
     print(openedtabs)
-    x=(input("please,enter the index of the tabs you want to display its content:"))
-    if x.isdigit():
-     index=int(x)
-     if openedtabs[index] in openedtabs:   #searching for the index given by the user
+    y=(input("please,enter the index of the tabs you want to display its content:"))
+    if y.isdigit(): #O(n) n:invalid y
+     index=int(y)
+     if openedtabs[index] in openedtabs:   #searching for the index given by the user O(n) n:length of the list
         r=requests.get(openedtabs[index]['url'])      #send a request to get the content from the url, resources:youtube,w3schools,geeksforgeeks
         if (r.status_code==200):                      #checking if the request is accepted
          print(r.text)
@@ -50,22 +50,22 @@ def Switch_Tabs(): #first printing the list of the opened tabs than asking the u
        print("please you have to give us the index as integer")
        Switch_Tabs()     
 
-def Display_All_Tabs(openedtabs): #searching for title in openedtabs and displaying it,i didn't know how to figure out the nested titles
-    for i in openedtabs:
+def Display_All_Tabs(openedtabs): #searching for title in openedtabs and displaying it,i didn't know how to figure out the nested titles #O(n)n:length of openedtabs
+    for i in openedtabs:    
         print(i.get('title'))
           
-def Open_Nested_Tabs(openedtabs): 
-    y=(input("please enter the index of the parent tab:"))
-    if y.isdigit():
+def Open_Nested_Tabs(openedtabs):    #O(n^3)
+    y=(input("please enter the index of the parent tab:"))   
+    if y.isdigit():    #O(n) n:number of invalid inputs
      index=int(y)  
-     for index in openedtabs: #creating another dictionary in the index of the dictionary given by the user
+     for index in openedtabs: #creating another dictionary in the index of the dictionary given by the user  O(n) n:length of openedtabs
         title=input("give us the title of the tab:")
-        if title and title.isalnum():
+        if title and title.isalnum():      #O(n) n:being invalid input
             url=input("please enter the URL:")
-            if url and url.startswith(("www.","http:","https:")):
+            if url and url.startswith(("www.","http:","https:")):  #O(1)
                 nestedtabs={"title":title,"url":url}
             else :
-                url=input("please enter the url again:") 
+                url=input("please enter the url again or it won't be opened:")  #O(1)
         else :
             print("please input a valid Title!")
             Open_Nested_Tabs(openedtabs)  
@@ -73,11 +73,11 @@ def Open_Nested_Tabs(openedtabs):
        print("please give us the index as integer")
        Open_Nested_Tabs()                 
 
-def Sort_All_Tabs(openedtabs):  #using selection method to sort the openedtabs alphabetically 
+def Sort_All_Tabs(openedtabs):  #using selection method to sort the openedtabs alphabetically #O(n^2)
     border=0
-    while border<len(openedtabs)-1:
+    while border<len(openedtabs)-1:  #O(n) n:being the length of the list
       minindex=border  #index of the minimum element
-      for i in range (border+1,len(openedtabs)):  
+      for i in range (border+1,len(openedtabs)):   #O(n) n:to find the minimum of the minimum element
         if openedtabs[i]['title']<openedtabs[minindex]['title']:
             minindex=i
       temp=openedtabs[border]  #swap the two elements
@@ -86,18 +86,19 @@ def Sort_All_Tabs(openedtabs):  #using selection method to sort the openedtabs a
       border+=1
     print(openedtabs)        
 
-def Save_Tabs(openedtabs):
+def Save_Tabs(openedtabs): #O(1)
      file_path=input("enter the file path:")
      with open(file_path, 'w') as f:
          json.dump(openedtabs,f)     #just calling json to save the path given by the user
          print(f"saved path into {file_path}")
 
-def Import_Tabs(openedtabs):
+def Import_Tabs(openedtabs): #O(1)
     file_path=input("enter the file path:")
     with open(file_path) as p:
         read=json.load(p)   #loading the file
         print(read)         #printing the content
 def mainmenu():
+    print("Hello user!")
     choice=0
     while (choice!=9):     #O(n) n being the number of invalid values given by the user 
         print("1.Open Tab.")
